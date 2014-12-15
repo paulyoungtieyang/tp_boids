ec79627c01af4fa988321b9ceccb96790d6405c7
paulyoungtieyang <pyoung.yty@gmail.com>
2014-12-15 17:20:31 +0100

    getters implemented and tested

--- main.cpp
+++ main.cpp
@@ -14,20 +14,24 @@
 #include <stdio.h>
 
 
-
 // ===========================================================================
 //                             Include Project Files
 // ===========================================================================
-#include "Agent.h"; 
+#include "Agent.h"
+
+// ===========================================================================
+//                               Static Attributes
+// ===========================================================================
+
+
+unsigned int Agent::PERCEPTION = 0;
+unsigned int Agent::CONTACT = 0;
 
 
 // ===========================================================================
 //                         Declare Miscellaneous Functions
 // ===========================================================================
-
-
-
-
+void test_getters_agent(Agent& my_test_agent);
 
 
 
@@ -39,6 +43,11 @@ int main(int argc, char* argv[])
 {
   printf("Hello World !\n");
 
+  Agent* Agent_1 = new Agent();
+  
+  //test des getters sur Agent
+  test_getters_agent(*Agent_1);
+
   return 0;
 }
 
@@ -48,3 +57,18 @@ int main(int argc, char* argv[])
 //                         Define Miscellaneous Functions
 // ===========================================================================
 
+void test_getters_agent(Agent& my_test_agent){
+	printf("Posx %ld \n", my_test_agent.Get_posX());
+	printf("Posy%ld \n", my_test_agent.Get_posY());
+	
+	printf("Spx %ld \n", my_test_agent.Get_spX());
+	printf("Spy %ld \n", my_test_agent.Get_spY());
+
+	printf("y1 %ld \n", my_test_agent.Get_y1());
+	printf("y2 %ld \n", my_test_agent.Get_y2());
+    printf("y3 %ld \n", my_test_agent.Get_y3());
+
+    printf("Perception %ld \n", my_test_agent.Get_Perception());
+    printf("Contact %ld \n", my_test_agent.Get_Contact());
+
+}
